c249d9b90c148a52ed46e36fd7788dc6dbd99300
joan <joanbarrosogarrido@gmail.com>
2020-03-30 22:44:58 +0200

    add empty viewholder

---
+++ app/src/main/java/com/s/coronavirus/adapter/CountryOverviewViewHolder.kt
@@ -0,0 +1,11 @@
+package com.s.coronavirus.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CountryOverviewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+  fun bind(item: Unit) {
+    // TODO
+  }
+}
