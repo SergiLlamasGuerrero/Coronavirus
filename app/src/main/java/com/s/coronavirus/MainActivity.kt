0717b4b03a03597e87c7a8cac8ac986e6ca2bac9
joan <joanbarrosogarrido@gmail.com>
2020-03-30 22:44:58 +0200

    add empty viewholder

--- app/src/main/java/com/s/coronavirus/MainActivity.kt
+++ app/src/main/java/com/s/coronavirus/MainActivity.kt
@@ -2,11 +2,32 @@ package com.s.coronavirus
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.s.coronavirus.adapter.OverviewAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+  private val adapter: OverviewAdapter by lazy { OverviewAdapter() }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    initRecyclerView()
+    
+    showData(List(10) {Unit})
+  }
+
+  private fun initRecyclerView() {
+    countries_overview_rv.layoutManager =
+      LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+  }
+
+  private fun showData(data: List<Unit>) {
+    if (countries_overview_rv.adapter == null)
+      countries_overview_rv.adapter = adapter
+
+    adapter.items = data
   }
 }
---
+++ app/src/main/java/com/s/coronavirus/adapter/OverviewAdapter.kt
@@ -0,0 +1,26 @@
+package com.s.coronavirus.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.s.coronavirus.R
+
+class OverviewAdapter : RecyclerView.Adapter<CountryOverviewViewHolder>() {
+
+  var items: List<Unit> = listOf()
+    set(value) {
+      field = value
+      notifyDataSetChanged()
+    }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryOverviewViewHolder {
+    return CountryOverviewViewHolder(LayoutInflater.from(parent.context)
+      .inflate(R.layout.list_item_country_overview, parent, false))
+  }
+
+  override fun getItemCount(): Int = items.size
+
+  override fun onBindViewHolder(holder: CountryOverviewViewHolder, position: Int) {
+    holder.bind(items[position])
+  }
+}
