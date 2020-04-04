c58e89413e00e98506f5e74816220f04f9d1a849
joan <joanbarrosogarrido@gmail.com>
2020-04-04 17:30:31 +0200

    render data and change cell design

--- app/src/main/java/com/s/coronavirus/adapter/CountryOverviewViewHolder.kt
+++ app/src/main/java/com/s/coronavirus/adapter/CountryOverviewViewHolder.kt
@@ -3,10 +3,15 @@ package com.s.coronavirus.adapter
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.s.coronavirus.adapter.entities.OverviewData
+import kotlinx.android.synthetic.main.list_item_country_overview.view.*
 
 class CountryOverviewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
   fun bind(item: OverviewData) {
-    // TODO
+    with(itemView) {
+      name_texView.text = item.countryName
+      deaths_textView.text = "Deaths: ${item.deaths}"
+      confirmed_textView.text = "Confirmed: ${item.confirmed}"
+    }
   }
 }
